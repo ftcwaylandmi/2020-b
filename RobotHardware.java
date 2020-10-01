c120014ee235f5d1c533986873d672bec6bc0beb
Wayland FTC Robotics <ftc@waylandunion.org>
2020-10-01 19:01:38 -0400

    Robot Hardware

---
+++ RobotHardware.java
@@ -0,0 +1,42 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+
+public class RobotHardware {
+
+    public DcMotor leftfrontDrive = null;
+    public DcMotor rightfrontDrive = null;
+    public DcMotor leftbackDrive = null;
+    public DcMotor rightbackDrive = null;
+
+    HardwareMap hwMap = null;
+
+    public RobotHardware () {
+
+    }
+
+    public void init (HardwareMap ahwMap) {
+        hwMap = ahwMap;
+
+        leftfrontDrive = hwMap.get(DcMotor.class, "lfd");
+        rightfrontDrive = hwMap.get(DcMotor.class, "rfd");
+        leftbackDrive = hwMap.get(DcMotor.class, "lbd");
+        rightbackDrive = hwMap.get(DcMotor.class, "rbd");
+        leftfrontDrive.setDirection(DcMotor.Direction.FORWARD);
+        rightfrontDrive.setDirection(DcMotor.Direction.REVERSE);
+        leftbackDrive.setDirection(DcMotor.Direction.FORWARD);
+        rightbackDrive.setDirection(DcMotor.Direction.REVERSE);
+        leftfrontDrive.setPower(0);
+        rightfrontDrive.setPower(0);
+        leftbackDrive.setPower(0);
+        rightbackDrive.setPower(0);
+        leftfrontDrive.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        rightfrontDrive.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        leftbackDrive.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        rightbackDrive.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+
+    }
+
+}
