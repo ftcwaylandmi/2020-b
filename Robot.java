25db79c4b56920233c2745baf5ec48698b676a6a
Wayland FTC Robotics <ftc@waylandunion.org>
2020-10-06 19:11:56 -0400

    Start of Robot code

--- Robot.java
+++ Robot.java
@@ -1,4 +1,42 @@
 package org.firstinspires.ftc.teamcode;
 
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import org.firstinspires.ftc.teamcode.RobotHardware;
+
 public class Robot {
+
+    RobotHardware myself = new RobotHardware();
+
+    public void initHW(HardwareMap ahwMap){
+        myself.init(ahwMap);
+    }
+
+    public void Drive(double s) {
+        myself.leftfrontDrive.setPower(s);
+        myself.rightfrontDrive.setPower(s);
+        myself.leftbackDrive.setPower(s);
+        myself.rightbackDrive.setPower(s);
+    }
+
+    public void StopDrive(double s){
+        myself.leftfrontDrive.setPower(0);
+        myself.rightfrontDrive.setPower(0);
+        myself.leftbackDrive.setPower(0);
+        myself.rightbackDrive.setPower(0);
+    }
+
+    public void SpinRight(double s){
+        myself.leftfrontDrive.setPower(s);
+        myself.rightfrontDrive.setPower(-s);
+        myself.leftbackDrive.setPower(s);
+        myself.rightbackDrive.setPower(-s);
+    }
+
+    public void SpinLeft(double s) {
+        myself.leftfrontDrive.setPower(-s);
+        myself.rightfrontDrive.setPower(s);
+        myself.leftbackDrive.setPower(-s);
+        myself.rightbackDrive.setPower(s);
+    }
+
 }
